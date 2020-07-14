730311ae06d667068c747d367d032408184b901d
DraeRamsey <andrea.jos.ramsey@gmail.com>
2020-07-13 19:09:36 -0700

    added pre-game and game sections

--- js/script.js
+++ js/script.js
@@ -1,1 +1,35 @@
-console.log("hello");
+'use strict';
+
+var play_btn = document.getElementById('play');
+var greetings = document.getElementById('greetings');
+
+var playing = false;
+
+
+play_btn.addEventListener('click', function()
+{
+    if (playing == false)
+    {
+      greetings.style.top = '-100vh';
+      playing = true;
+    }
+
+   else {
+     greetings.style.top = '0';
+     playing = false;
+   }
+
+
+
+});
+
+//if you want to do something on load
+window.addEventListener( "load", function()
+{
+
+});
+
+
+
+
+//create keyboard
